10a0afb886c1bcacb23f301839f597a0ca60f594
cyrilmichino <cyrilmichino@gmail.com>
2025-02-18 23:57:59 +0300

    Adjust Main Nav in All Pages

--- src/main.js
+++ src/main.js
@@ -2,9 +2,3 @@ function toggleMenu() {
     var navbar = document.getElementById("main-menu");
     navbar.classList.toggle("hide-mobile");
 }
-
-// const hamburger = document.getElementById("hamburger");
-// hamburger.addEventListener('click', toggleMenu());
-
-// const closeToggle = document.getElementById("close-toggle");
-// closeToggle.addEventListener('click', toggleMenu());
