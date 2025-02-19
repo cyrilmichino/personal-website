5e20ea9b3581642e41acf4635e755be4f38d597d
cyrilmichino <cyrilmichino@gmail.com>
2025-02-19 18:03:24 +0300

    Style homepage banner with typed text

--- src/main.js
+++ src/main.js
@@ -4,3 +4,10 @@ function toggleMenu() {
     navbar.classList.toggle("hide-mobile");
     overlay.classList.toggle("hide-mobile");
 }
+
+var typed = new Typed('#toggle-text', {
+    strings: ['a Data Scientist', 'an Entrepreneur','a Data Consultant'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+  });
